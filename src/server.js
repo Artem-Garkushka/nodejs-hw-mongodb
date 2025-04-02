c624ac3d0b94cd80cc6c321bed16c35a3c784942
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-04-02 20:53:04 +0300

    noda

--- src/server.js
+++ src/server.js
@@ -1,16 +1,15 @@
-import { getAllContacts, getContactById } from './services/contacts.js';
-import cors from 'cors';
 import express from 'express';
-import { getEnvVar } from './utils/getEnvVar.js';
 import pino from 'pino-http';
+import cors from 'cors';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
-const PORT = Number(getEnvVar('PORT', '3000'));
+const PORT = Number(getEnvVar('PORT', 3000));
 
 export const setupServer = () => {
   const app = express();
 
   app.use(express.json());
-
   app.use(cors());
 
   app.use(
@@ -21,15 +20,8 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/', (req, res) => {
-    res.json({
-      message: 'Hello world!',
-    });
-  });
-
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
-
     res.status(200).json({
       status: 200,
       message: 'Successfully found contacts!',
@@ -37,7 +29,7 @@ export const setupServer = () => {
     });
   });
 
-  app.get('/contacts/:contactId', async (req, res, next) => {
+  app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
     const contact = await getContactById(contactId);
 
@@ -54,7 +46,8 @@ export const setupServer = () => {
       data: contact,
     });
   });
-  app.use((req, res, next) => {
+
+  app.use('*', (req, res, next) => {
     res.status(404).json({
       message: 'Not found',
     });
