5f66529a3bd88dd95e003c6bb9de334caa580abf
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-05-01 23:50:12 +0300

    node

--- src/server.js
+++ src/server.js
@@ -1,13 +1,42 @@
-import { setupServer } from './server.js';
-import { initMongoConnection } from './db/initMongoConnection.js';
-import { createDirIfNotExists } from './utils/createDirIfNotExists.js';
-import { TEMP_UPLOAD_DIR, UPLOAD_DIR } from './constants/index.js';
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import router from './routers/index.js';
+import { getEnvVar } from './utils/getEnvVar.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import cookieParser from 'cookie-parser';
 
-const bootstrap = async () => {
-  await initMongoConnection();
-  await createDirIfNotExists(TEMP_UPLOAD_DIR);
-  await createDirIfNotExists(UPLOAD_DIR);
-  setupServer();
+const PORT = Number(getEnvVar('PORT', 3000));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
+    }),
+  );
+  app.use(cors());
+
+  app.use(cookieParser());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use(router);
+
+  app.use('*', notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 };
-
-void bootstrap();
