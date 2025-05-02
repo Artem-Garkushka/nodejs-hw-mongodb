62f622471acac092d5c2d7c038e6a38c72089b48
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-05-03 00:50:36 +0300

    node

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -8,13 +8,17 @@ import {
 } from '../services/auth.js';
 import { THIRTY_DAYS } from '../constants/index.js';
 
-export const registerUserController = async (req, res) => {
-  const user = await registerUser(req.body);
-  res.status(201).json({
-    status: 201,
-    message: 'Successfully registered a user!',
-    data: user,
-  });
+export const registerUserController = async (req, res, next) => {
+  try {
+    const user = await registerUser(req.body);
+    res.status(201).json({
+      status: 201,
+      message: 'Successfully registered a user!',
+      data: user,
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const loginUserController = async (req, res) => {
