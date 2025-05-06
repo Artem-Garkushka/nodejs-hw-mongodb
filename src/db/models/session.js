2ebce298603805357eeb5df00b0330d9a4ac3390
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-05-07 00:29:49 +0300

    node

--- src/db/models/session.js
+++ src/db/models/session.js
@@ -2,30 +2,13 @@ import { model, Schema } from 'mongoose';
 
 const sessionSchema = new Schema(
   {
-    userId: {
-      type: Schema.Types.ObjectId,
-      ref: 'users',
-      required: true,
-    },
-    accessToken: {
-      type: String,
-      required: true,
-    },
-    refreshToken: {
-      type: String,
-      required: true,
-    },
-    accessTokenValidUntil: {
-      type: Date,
-      required: true,
-    },
-
-    refreshTokeValidUntil: {
-      type: Date,
-      required: true,
-    },
+    userId: { type: String, required: true },
+    accessToken: { type: String, required: true },
+    refreshToken: { type: String, required: true },
+    accessTokenValidUntil: { type: Date, required: true },
+    refreshTokenValidUntil: { type: Date, required: true },
   },
-  { versionKey: false, timestamps: true },
+  { timestamps: true, versionKey: false },
 );
 
 export const SessionCollection = model('session', sessionSchema);
