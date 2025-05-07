7fbefe17b2cdfc1ca821653dba77d5b02e91179e
Artem Garkushka <Artem.Garkushka@gmail.com>
2025-05-07 17:10:07 +0300

    node

--- src/db/models/user.js
+++ src/db/models/user.js
@@ -9,38 +9,14 @@ const userSchema = new Schema(
         minlength: 3,
         maxlength: 20,
       },
-      phoneNumber: {
-        type: String,
-        required: true,
-        minlength: 3,
-        maxlength: 20,
-      },
       email: {
         type: String,
       },
-      isFavourite: {
-        type: Boolean,
-        default: false,
-        required: true,
-      },
-      contactType: {
+      password: {
         type: String,
-        enum: typeList,
-        required: true,
-        default: typeList[0],
-      },
-      userId: {
-        type: Schema.Types.ObjectId,
-        ref: 'users',
         required: true,
       },
-      photo: {
-        type: String,
-        required: false,
-        default: null,
-      },
-    },
-    { versionKey: false, timestamps: true },
+    }
   );
 
 userSchema.methods.toJSON = function () {
